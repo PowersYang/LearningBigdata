5c0b1b4f693d6f842305367487ff15408e97cf83
Y-sir <577790911@qq.com>
2019-11-25 22:31:22 +0800

    update

--- Hadoop/src/main/java/com/ysir308/HDFSClient.java
+++ Hadoop/src/main/java/com/ysir308/HDFSClient.java
@@ -2,7 +2,9 @@ package com.ysir308;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.LocatedFileStatus;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.RemoteIterator;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -45,5 +47,58 @@ public class HDFSClient {
         // 3、关闭资源
         fs.close();
 
+    }
+
+    @Test
+    public void testCopyToLocalFile() throws IOException, URISyntaxException, InterruptedException {
+
+        // 1、获取fs对象
+        Configuration conf = new Configuration();
+        FileSystem fs = FileSystem.get(new URI("hdfs://localhost:9000"), conf, "ysir");
+
+        // 2、执行上传
+        fs.copyToLocalFile(new Path("/usr/ysir/水电费.xlsx"), new Path("/Users/ysir/temp/水电费.txt"));
+
+        // 3、关闭资源
+        fs.close();
+    }
+
+    @Test
+    public void testDelete() throws IOException, URISyntaxException, InterruptedException {
+        Configuration conf = new Configuration();
+        FileSystem fs = FileSystem.get(new URI("hdfs://localhost:9000"), conf, "ysir");
+
+        fs.delete(new Path("/usr/ysir"), true);
+
+        fs.close();
+    }
+
+    @Test
+    public void testRename() throws IOException, URISyntaxException, InterruptedException {
+        Configuration conf = new Configuration();
+        FileSystem fs = FileSystem.get(new URI("hdfs://localhost:9000"), conf, "ysir");
+
+        fs.rename(new Path("/usr/ysir2"), new Path("/usr/ysir"));
+
+        fs.close();
+    }
+
+    @Test
+    public void testListFiles()throws IOException, URISyntaxException, InterruptedException {
+        Configuration conf = new Configuration();
+        FileSystem fs = FileSystem.get(new URI("hdfs://localhost:9000"), conf, "ysir");
+
+        RemoteIterator<LocatedFileStatus> listFiles = fs.listFiles(new Path("/usr/ysir"), true);
+
+        while (listFiles.hasNext()){
+            LocatedFileStatus fileStatus = listFiles.next();
+            System.out.println(fileStatus.getPath().getName());
+            System.out.println(fileStatus.getPermission());
+            System.out.println(fileStatus.getLen());
+            System.out.println(fileStatus.getBlockSize());
+        }
+
+
+        fs.close();
     }
 }
