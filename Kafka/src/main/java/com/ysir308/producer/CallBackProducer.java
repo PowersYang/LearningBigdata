1e38e452b165e0b341e493292c9657c397d966fd
Y-sir <577790911@qq.com>
2019-12-13 16:24:20 +0800

    update

---
+++ Kafka/src/main/java/com/ysir308/producer/CallBackProducer.java
@@ -0,0 +1,39 @@
+package com.ysir308.producer;
+
+import org.apache.kafka.clients.producer.*;
+
+import java.util.Properties;
+
+public class CallBackProducer {
+
+    public static void main(String[] args) {
+
+        Properties prop = new Properties();
+
+        prop.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "hadoop102:9092");
+        prop.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+        prop.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+
+
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(prop);
+
+        for (int i = 0; i < 10; i++) {
+
+            producer.send(new ProducerRecord<String, String>("first", "ysir " + i), new Callback() {
+                public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+
+                    // 成功的话返回的是RecordMetadata，失败了返回的是Exception
+                    if (e == null) {
+                        System.out.println("-----------------------");
+                        System.out.println(recordMetadata.topic());
+                        System.out.println(recordMetadata.partition());
+                        System.out.println(recordMetadata.offset());
+                        System.out.println("-----------------------");
+                    }
+                }
+            });
+
+        }
+
+    }
+}
