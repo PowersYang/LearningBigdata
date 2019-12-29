060ac1cbf29fd995b4eeae951654950461f5b99a
Y-sir <577790911@qq.com>
2019-12-29 21:51:15 +0800

    update

---
+++ Spark/spark_streaming/demo.py
@@ -0,0 +1,23 @@
+import os
+
+from pyspark import SparkContext
+from pyspark.streaming import StreamingContext
+
+os.environ['PYSPARK_PYTHON'] = '/usr/local/bin/python3'
+
+sc = SparkContext('local[*]', 'WordCount')
+ssc = StreamingContext(sc, 1)
+
+lines = ssc.socketTextStream('localhost', 9999)
+
+words = lines.flatMap(lambda line: line.split(' '))
+
+pairs = words.map(lambda word: (word, 3))
+
+word_counts = pairs.reduceByKey(lambda x, y: x + y)
+
+# Print the first ten elements of each RDD generated in this DStream to the console
+word_counts.pprint()
+
+ssc.start()
+ssc.awaitTermination()
--- Spark/sparksql/demo.py
+++ Spark/sparksql/demo.py
@@ -3,26 +3,26 @@ import os
 from pyspark import SparkConf
 from pyspark.sql import SparkSession
 
-os.environ["PYSPARK_PYTHON"] = "/usr/local/bin/python3"
+os.environ['PYSPARK_PYTHON'] = '/usr/local/bin/python3'
 
 if __name__ == '__main__':
-    spark_conf = SparkConf().setAppName('SparkSql Demo').setMaster('spark://localhost:7077')
+    spark_conf = SparkConf().setAppName('SparkSql Demo').setMaster('local[*]')
 
-    spark = SparkSession.builder.getOrCreate()
+    spark = SparkSession.builder.config(conf=spark_conf).getOrCreate()
 
-    # df = spark.read.json('../in/user.json')
+    df = spark.read.json('../in/user.json')
 
-    # df.show()
-    #
-    # df.select('name').show()
-    #
-    # df.select(df['name'], df['age'] + 1).show()
+    df.show()
+
+    df.select('name').show()
+
+    df.select(df['name'], df['age'] + 1).show()
 
     # df.createGlobalTempView('students')
     #
     # spark.sql('select name, age from global_temp.students').show()
 
-    rdd = spark.sparkContext.parallelize([1, 2, 3, 4, 5])
-    rdd.foreach(lambda x: print(x))
+    # rdd = spark.sparkContext.parallelize([1, 2, 3, 4, 5])
+    # rdd.foreach(lambda x: print(x))
 
     spark.stop()
